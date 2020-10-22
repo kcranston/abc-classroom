cee025bb37c636e5b57e29c4b16ca16dd4e71174
Karen Cranston <karen.cranston@gmail.com>
2020-10-22 09:29:14 -0400

    testing scripts for authentication

--- githubapp/auth-test.py
+++ githubapp/auth-test.py
@@ -1,22 +1,58 @@
 import requests
 
-client_id = "Iv1.8df72ad9560c774c"
-header = {"Content-Type": "application/json", "Accept": "application/json"}
-postdata = {"client_id": client_id}
+from argparse import ArgumentParser
 
-# r = requests.post('https://github.com/login/device/code',data = postdata)
-# print(r.text)
 
-# device_code="44ca6ea747482bc5b338c383f4493da4a3d9632f"
-device_code = "ca4cd2ac633bf6fadbe56363f8ce9e2f1e11e0fc"
-payload = {
-    "client_id": client_id,
-    "device_code": device_code,
-    "grant_type:": "urn:ietf:params:oauth:grant-type:device_code",
-}
-r = requests.post(
-    "https://github.com/login/oauth/access_token", headers=header, json=payload
-)
-print(r.encoding)
-print(r.status_code)
-print(r.json())
+def get_login_code(client_id):
+
+    header = {"Content-Type": "application/json", "Accept": "application/json"}
+    payload = {"client_id": client_id}
+    link = "https://github.com/login/device/code"
+    r = requests.post(link, headers=header, json=payload)
+    print(r.json())
+    data = r.json()
+    device_code = data["device_code"]
+    uri = data["verification_uri"]
+    user_code = data["user_code"]
+
+    # prompt the user to enter the code
+    print(
+        "To authorize this app, go to {} and enter the code {}".format(
+            uri, user_code
+        )
+    )
+
+    return device_code
+
+
+def poll_for_status(client_id, device_code):
+    header = {"Content-Type": "application/json", "Accept": "application/json"}
+    payload = {
+        "client_id": client_id,
+        "device_code": device_code,
+        "grant_type:": "urn:ietf:params:oauth:grant-type:device_code",
+    }
+    r = requests.post(
+        "https://github.com/login/oauth/access_token",
+        headers=header,
+        json=payload,
+    )
+    print(r.json())
+
+
+if __name__ == "__main__":
+    parser = ArgumentParser()
+    parser.add_argument(
+        "client_id", help="""The client id of your github app."""
+    )
+    args = parser.parse_args()
+
+    # client_id = "Iv1.8df72ad9560c774c"
+    print(args.client_id)
+    device_code = get_login_code(args.client_id)
+
+    input(
+        "Press any key to continue once you have input the code successfully"
+    )
+
+    poll_for_status(args.client_id, device_code)
